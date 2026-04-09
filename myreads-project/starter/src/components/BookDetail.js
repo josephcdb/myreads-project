93d28e2ab0c1479bbd601207de67049cb72e9ad5
Joseph <chua.debao@gmail.com>
2026-04-09 17:16:13 +0200

    Add useNavigate on BookDetail to return back to main page

--- myreads-project/starter/src/components/BookDetail.js
+++ myreads-project/starter/src/components/BookDetail.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import * as BooksAPI from "../BooksAPI";
 
 const BookDetail = () => {
   const { id } = useParams();
   const [book, setBook] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     BooksAPI.get(id).then(setBook);
@@ -20,7 +21,7 @@ const BookDetail = () => {
       <p>{book.description}</p>
       <p>Published: {book.publishedDate}</p>
       <p>Average Rating: {book.averageRating}</p>
-      <button className="btn btn-primary" onClick={() => window.history.back()}>Return back to Main</button>
+      <button className="btn btn-primary" onClick={() => navigate("/")}>Return back to Main</button>
     </div>
   );
 };
