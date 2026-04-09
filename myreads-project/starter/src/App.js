29000ac3175b1686d78a7797ac0e417f137a808e
Joseph <chua.debao@gmail.com>
2026-04-09 13:28:19 +0200

    Create new component folder to add Header as a clean code

--- myreads-project/starter/src/App.js
+++ myreads-project/starter/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
   const [showSearchPage, setShowSearchPage] = useState(false);
@@ -28,9 +29,7 @@ function App() {
         </div>
       ) : (
         <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
+          <Header />
           <div className="list-books-content">
             <div>
               <div className="bookshelf">
---
+++ myreads-project/starter/src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
