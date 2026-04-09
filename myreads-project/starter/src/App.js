1685a0a82a07499bdf661cea5819bd59bbe7c96d
Joseph <chua.debao@gmail.com>
2026-04-09 15:20:07 +0200

    Use BookAPI to get the list of books instead of static array list of books

--- myreads-project/starter/src/App.js
+++ myreads-project/starter/src/App.js
@@ -1,91 +1,39 @@
-import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Bookshelf from "./components/Bookshelf";
 import SearchPage from "./components/SearchPage";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
 
 function App() {
   const [showSearchPage, setShowSearchPage] = useState(false);
+  const [books, setBooks] = useState([]);
 
-  const [books, setBooks] = useState([
-    {
-      id: "1",
-      title: "To Kill a Mockingbird",
-      authors: ["Harper Lee"],
-      link: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y",
-      width: 128,
-      height: 193,
-      shelf: "currentlyReading",
-    },
-    {
-      id: "2",
-      title: "Ender's Game",
-      authors: ["Orson Scott Card"],
-      link: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN",
-      width: 128,
-      height: 188,
-      shelf: "currentlyReading",
-    },
-    {
-      id: "3",
-      title: "1776",
-      authors: ["David McCullough"],
-      link: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX",
-      width: 128,
-      height: 193,
-      shelf: "wantToRead",
-    },
-    {
-      id: "4",
-      title: "Harry Potter and the Sorcerer's Stone",
-      authors: ["J.K. Rowling"],
-      link: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB",
-      width: 128,
-      height: 192,
-      shelf: "wantToRead",
-    },
-    {
-      id: "5",
-      title: "The Hobbit",
-      authors: ["J.R.R. Tolkien"],
-      link: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk",
-      width: 128,
-      height: 192,
-      shelf: "read",
-    },
-    {
-      id: "6",
-      title: "Oh, the Places You'll Go!",
-      authors: ["Seuss"],
-      link: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8",
-      width: 128,
-      height: 174,
-      shelf: "read",
-    },
-    {
-      id: "7",
-      title: "The Adventures of Tom Sawyer",
-      authors: ["Mark Twain"],
-      link: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi",
-      width: 128,
-      height: 192,
-      shelf: "read",
-    }
-  ]);
+  useEffect(() => {
+    BooksAPI.getAll().then((books) => {
+      setBooks(books);
+    });
+  }, []);
   
   const currentlyReadingBooks = books.filter(book => book.shelf === "currentlyReading");
   const wantToReadBooks = books.filter(book => book.shelf === "wantToRead");
   const readBooks = books.filter(book => book.shelf === "read");
 
   const handleChangeShelf = (book, newShelf) => {
-    const updated = books.filter((b) => b.id !== book.id);
-    setBooks([...updated, { ...book, shelf: newShelf }]);
+    BooksAPI.update(book, newShelf).then(() => {
+      const updated = books.filter((b) => b.id !== book.id);
+      setBooks([...updated, { ...book, shelf: newShelf }]);
+    });
   };
 
   return (
     <div className="app">
       { showSearchPage ? 
-        (<SearchPage onClose={() => setShowSearchPage(false)} />) :
+        (<SearchPage 
+          onClose={() => setShowSearchPage(false)}
+          onChangeShelf={handleChangeShelf}
+          myBooks={books}
+        />) :
       (
         <>
           <div className="list-books">
