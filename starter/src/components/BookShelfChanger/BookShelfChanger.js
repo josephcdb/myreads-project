7789024cb6d1d91d4d271b061063a559a73e420b
Joseph <chua.debao@gmail.com>
2026-04-11 09:00:33 +0200

    Fix the issue by changing the value to move because of duplicate value - none

--- starter/src/components/BookShelfChanger/BookShelfChanger.js
+++ starter/src/components/BookShelfChanger/BookShelfChanger.js
@@ -5,9 +5,7 @@ const BookShelfChanger = ({ onChange, currentShelf, bookId }) => {
     return (
         <div className="bookshelfchanger">
             <select id={`shelf-${bookId}`} name={`shelf-${bookId}`} value={currentShelf} onChange={(e) => onChange(e.target.value)}>
-                <option value="none" disabled>
-                    Move to...
-                </option>
+                <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
